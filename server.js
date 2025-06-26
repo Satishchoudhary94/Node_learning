180b872dedf681369a4d9a6b2625f483e5ba5906
Your Name <your.email@example.com>
2025-06-26 18:22:52 +0530

    Routing by Node js

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
  
-  res.end('Hello World\n');
+  const url = req.url;
+
+  if(url == '/'){
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Welcome to the Home Page</h1>');
+  }
+
+  else if(url == '/contact'){
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Contact Us at contact@example.com</h1>');
+  }
+
 });
+
+
 
 server.listen(3000, () => {
   console.log('Server is running at http://localhost:3000/');
